80d166bf88a4759179335957b68b92f1da4337a1
Steve Conover <sconover@gmail.com>
2017-07-21 12:40:42 +0200

    minecraft startup script varies based on variant specified. also, make it so startup commands run whenever the minecraft server is ready, on restarts.

--- lib/const.py
+++ lib/const.py
@@ -3,3 +3,6 @@ MINECRAFT_SERVER_JAR_NAME = "sponge-minecraft-server.jar"
 SPIGOT_LATEST_JAR_LOCATOR_COMMAND = "ls -r ~/spigot-buildtools/spigot-*.jar | head -n 1 | xargs readlink -f"
 GRPC_CRAFT_PLUGIN_JAR_LOCATOR_COMMAND = "ls -r ~/grpc-craft/plugin/target/plugin-*.jar | head -n 1 | xargs readlink -f"
 PYTHON_VIRTUALENV=".python-virtualenv"
+
+MINECRAFT_SERVER_VARIANT_SPIGOT_GRPC="spigot_grpc"
+MINECRAFT_SERVER_VARIANT_SPONGE_MCPI="sponge_mcpi"
