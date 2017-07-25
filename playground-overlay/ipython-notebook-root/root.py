0723997781ca05871c1d4e22cc89e92d8cd791ec
Steve Conover <sconover@gmail.com>
2017-07-25 10:55:46 +0200

    write ipython nginx fragment and reload nginx

--- playground-overlay/ipython-notebook-root/root.py
+++ playground-overlay/ipython-notebook-root/root.py
@@ -1,9 +1,10 @@
 import sys
 import os
+import subprocess
 
+this_dir = os.path.dirname(os.path.realpath(__file__))
 
 def _get_root_conf_value(key):
-    this_dir = os.path.dirname(os.path.realpath(__file__))
     contents = dict(line.strip().split('=') for line in open(os.path.join(this_dir,'root.conf')))
     value = contents[key]
     assert value!=None and len(value) > 0
@@ -22,3 +23,31 @@ def get_grpc_craft_port():
 	return int(_get_root_conf_value('grpc_craft_port'))
 
 sys.stderr.write("LOADING IPYTHON ENVIORNMENT {}: PLAYER={} PORT={}\n".format(get_env_name(), get_minecraft_player_name(), get_port()))
+
+def nginx_conf_content():
+        lines = [
+            "location /programs/{}/ {{".format(get_env_name()), # double {{ to escape for interpolation
+            "  proxy_pass http://127.0.0.1:{};".format(get_port()),
+            "  proxy_http_version 1.1;",
+            "  proxy_set_header Upgrade $http_upgrade;",
+            "  proxy_set_header Connection \"upgrade\";",
+            "  proxy_set_header Origin \"\";",
+            "}"
+        ]
+        return "\n".join(lines) + "\n\n"
+
+sys.stderr.write("local nginx.conf:\n")
+sys.stderr.write(nginx_conf_content())
+
+def write_nginx_conf():
+    nginx_conf_path = os.path.join(this_dir,'nginx.conf')
+    f = open(nginx_conf_path, 'w')
+    f.write(nginx_conf_content())
+    f.close()
+
+write_nginx_conf()
+
+def reload_nginx():
+    print(subprocess.check_output("sudo /etc/init.d/nginx reload 2>&1", shell=True))
+
+reload_nginx()
